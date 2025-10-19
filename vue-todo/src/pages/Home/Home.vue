e88a024828a2bdd52c01bf380f6225d303e0c373
phamLinh98 <linhthusinh98@gmail.com>
2025-10-19 19:34:36 +0900

    update

--- vue-todo/src/pages/Home/Home.vue
+++ vue-todo/src/pages/Home/Home.vue
@@ -1,3 +1,23 @@
-<template src="./Home.html"></template>
-<script setup src="./Home.js"></script>
+import Test from './Test.vue'
+
+<template src="./Home.html">
+    <Test :message="msg" />
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const newTodo = ref('')
+const todos = ref([])
+
+const addTodo = () =>  {
+  if (newTodo.value.trim()) {
+    todos.value.push(newTodo.value)
+    newTodo.value = ''
+  }
+}
+
+const removeTodo = (index) => {
+  todos.value.splice(index, 1)
+}
+</script>
 <style scoped src="./Home.css"></style>
---
+++ vue-todo/src/pages/Home/Test.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>{{ message }}</p>
+</template>
+
+<script setup>
+defineProps({
+  message: String
+})
+</script>
